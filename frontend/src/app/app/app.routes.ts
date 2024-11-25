03b3c815609001a6a5b6d0c0dc5b7d4d5f9732d4
Miguel <miguel.suarezo@autonoma.edu.co>
2024-11-25 17:13:44 -0500

    Endpoint para filtrar eventos por categoría y su conexión inicial con la página de eventos (todavía no sirve)

--- frontend/src/app/app/app.routes.ts
+++ frontend/src/app/app/app.routes.ts
@@ -1,5 +1,3 @@
-// src/app/app.routes.ts
-
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component'; // Adjust based on actual component paths
 import { AboutComponent } from '../pages/about/about.component';
@@ -7,11 +5,12 @@ import { LoginComponent } from '../pages/login/login.component';
 import { RegisterComponent } from '../pages/register/register.component';
 import { ScheduleEventComponent } from '../components/schedule-event/schedule-event.component';
 import { CategoriesComponent } from '../pages/categories/categories.component';
+import { EventViewComponent } from '../pages/event-view/event-view.component';
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },  // Default route
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'categories', component: CategoriesComponent}
-
+  { path: 'categories', component: CategoriesComponent},
+  { path: ':cat_name', component: EventViewComponent}
 ];
---
+++ frontend/src/app/pages/event-view/event-view.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-event-view',
+  standalone: true,
+  imports: [HttpClientModule, HeaderComponent,FormsModule,CommonModule],
+  templateUrl: './event-view.component.html',
+  styleUrl: './event-view.component.scss'
+})
+export class EventViewComponent {
+  cat_name!: string;
+  events: any[] = [];
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.cat_name = params['cat_name'];
+      this.fetchEvents();
+    });
+  }
+
+  fetchEvents(): void {
+    this.http.get(`http://localhost:5000/api/eventos/${this.cat_name}`)
+      .subscribe(
+        (data: any) => this.events = data,
+        error => console.error('Error fetching events:', error)
+      );
+  }
+
+}
