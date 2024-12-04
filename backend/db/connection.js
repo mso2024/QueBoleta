8917f88e99e60d808f7c306af2031b6a25834f79
elcesar172006 <cesard.ariasp@autonoma.edu.co>
2024-12-04 16:20:42 -0500

    Se modifico el login

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost', // Replace with your DB host
   user: 'root',      // Replace with your DB username
-  password: '2486', // Replace with your DB password
-  database: 'QueBoleta',    // Replace with your database name
+  password: 'canela172006', // Replace with your DB password
+  database: 'queboleta',    // Replace with your database name
 });
 
 module.exports = pool.promise();
